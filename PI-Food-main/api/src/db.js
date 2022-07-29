f774b6daafdde69f2aa62889994e7c74e94dc9a5
NurCord <nunyypin@gmail.com>
2022-07-29 18:37:57 -0300

    deploy db finished1

--- PI-Food-main/api/src/db.js
+++ PI-Food-main/api/src/db.js
@@ -4,7 +4,6 @@ const path = require('path');
 /* const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env; */
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
