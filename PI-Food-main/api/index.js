62cc81173d122eab916d966ea44c0973826c2b5c
NurCord <nunyypin@gmail.com>
2022-07-29 19:05:44 -0300

    deploy db finished now yes

--- PI-Food-main/api/index.js
+++ PI-Food-main/api/index.js
@@ -22,11 +22,11 @@ const { conn } = require('./src/db.js');
 const { Diet } = require('./src/db')
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, async() => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
     //Pre-cargo los tipos de dietas en la base de datos
-    const dietas = [
+    /* const dietas = [
       {name:  "dairy free" , definition: 'A dairy-free diet excludes all dairy products. This category includes milk from any animal, as well as any product made from this milk, such as cheese, yogurt, butter, and cream. Yet, people who follow this eating pattern may still eat other animal foods like meat, fish, shellfish, and eggs.'},
       {name: "gluten free" , definition: 'Eliminating gluten means avoiding wheat, barley, rye, and other gluten-containing grains and foods made from them (or that may have been cross contaminated).'},
       {name: 'lacto ovo vegetarian', definition: 'All ingredients must be vegetarian,none of the ingredients can be or contain dairy and none of the ingredients can be or contain egg.'},
@@ -39,7 +39,7 @@ conn.sync({ force: true }).then(() => {
       {name: "ketogenic", definition: 'The keto diet is based more on the ratio of fat, protein, and carbs in the diet rather than specific ingredients. Generally speaking, high fat, protein-rich foods are acceptable and high carbohydrate foods are not. The formula we use is 55-80% fat content, 15-35% protein content, and under 10% of carbohydrates.'},
     ];
     await dietas.forEach(async (element) => await Diet.create({ name: element.name, definition: element.definition}));
-    console.log("Tipos de dieta pre-cargadas");
+    console.log("Tipos de dieta pre-cargadas"); */
   });
 });
 
